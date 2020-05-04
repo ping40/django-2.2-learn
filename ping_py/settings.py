acd9c344a5e5c18f6ce8f8e248ca896d2d874a43
ping40 <norberthu30@gmail.com>
2020-05-04 21:19:24 +0800

    part 7

--- ping_py/settings.py
+++ ping_py/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ping_py.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
